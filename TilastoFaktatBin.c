7e18a628919bf1b35a9d18226773c905fe5f20b1
Johnez112 <joona.pigg@student.lut.fi>
2025-03-23 19:09:02 +0200

    Lisätty apufunktioita

--- TilastoFaktatBin.c
+++ TilastoFaktatBin.c
@@ -6,30 +6,86 @@
 
 // Luodaan apufuktiot eri tilastofaktoja varten.
 static void laskeKeskiarvo(BNODE *pNode, int *iLkm, int *iSumma) {
-    if (pNode == NULL) return;
+
+
     (*iLkm)++;
+
     (*iSumma) += pNode->count;
+
     laskeKeskiarvo(pNode->pVasen, iLkm, iSumma);
     laskeKeskiarvo(pNode->pOikea, iLkm, iSumma);
 }
 
-static void laskeYliKeskiarvon(BNODE *pNode, double fKeskiarvo, int *iYli) {
-    if (pNode == NULL) return;
-    if (pNode->count > fKeskiarvo) (*iYli)++;
+static void laskeYliKeskiarvon(BNODE *pNode, double fKeskiarvo, int *iYli){
+
+
+    if (pNode->count > fKeskiarvo) {
+        (*iYli)++;
+        }
+
+
     laskeYliKeskiarvon(pNode->pVasen, fKeskiarvo, iYli);
     laskeYliKeskiarvon(pNode->pOikea, fKeskiarvo, iYli);
 }
 
 static void laskeAlleKeskiarvon(BNODE *pNode, double fKeskiarvo, int *iAli) {
-    if (pNode == NULL) return;
-    if (pNode->count < fKeskiarvo) (*iAli)++;
+ 
+
+    if (pNode->count < fKeskiarvo){
+            (*iAli)++;
+    } 
+
     laskeAlleKeskiarvon(pNode->pVasen, fKeskiarvo, iAli);
+
     laskeAlleKeskiarvon(pNode->pOikea, fKeskiarvo, iAli);
+
 }
 
 static void laskeParittomat(BNODE *pNode, int *iLkm) {
-    if (pNode == NULL) return;
-    if (pNode->count % 2 != 0) (*iLkm)++;
+
+    
+    if (pNode->count % 2 != 0){
+    (*iLkm)++;
+    } 
+
     laskeParittomat(pNode->pVasen, iLkm);
+
     laskeParittomat(pNode->pOikea, iLkm);
+}
+
+
+
+
+static void laskeParilliset(BNODE *pNode, int *iLkm) {
+
+    if (pNode->count % 2 == 0) {
+        (*iLkm)++;
+    }
+    laskeParilliset(pNode->pVasen, iLkm);
+
+
+    laskeParilliset(pNode->pOikea, iLkm);
+}
+
+
+static void etsiYleisin(BNODE *pNode, int *max, char *cKirjain, char *yleisinNimi) {
+
+    if (pNode->count > *max) {
+        *max = pNode->count;
+        *cKirjain = pNode->name[0];
+        strcpy(yleisinNimi, pNode->name);
+    }
+    etsiYleisin(pNode->pVasen, max, cKirjain, yleisinNimi);
+    etsiYleisin(pNode->pOikea, max, cKirjain, yleisinNimi);
+}
+
+static void laskeSamatAlkukirjaimet(BNODE *pNode, char cKirjain, int *iLkm) {
+    if (pNode->name[0] == cKirjain){
+        (*iLkm)++;
+
+    } 
+    laskeSamatAlkukirjaimet(pNode->pVasen, cKirjain, iLkm);
+
+    
+    laskeSamatAlkukirjaimet(pNode->pOikea, cKirjain, iLkm);
 }
